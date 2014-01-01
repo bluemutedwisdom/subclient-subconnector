b70739233fb3da25642f815875684dec4aef2426
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-01-01 13:06:42 +0100

    Defined method versions at ApiMethod enum

--- src/main/java/net/subclient/subsonic/api/ApiMethod.java
+++ src/main/java/net/subclient/subsonic/api/ApiMethod.java
@@ -21,6 +21,8 @@
 
 package net.subclient.subsonic.api;
 
+import net.subclient.subsonic.util.Version;
+
 /**
  * List of Subsonic API methods supported by Subconnector
  * @author Alejandro Celaya Alastrué
@@ -28,41 +30,49 @@ package net.subclient.subsonic.api;
  */
 public enum ApiMethod {
 	
-	PING				(String.format(ApiMethod.REST_PREFIX, "ping.view")),
-	GET_LICENSE			(String.format(ApiMethod.REST_PREFIX, "getLicense.view")),
-	GET_MUSIC_FOLDERS	(String.format(ApiMethod.REST_PREFIX, "getMusicFolders.view")),
-	GET_INDEXES			(String.format(ApiMethod.REST_PREFIX, "getIndexes.view")),
-	GET_MUSIC_DIRECTORY	(String.format(ApiMethod.REST_PREFIX, "getMusicDirectory.view")),
-	SEARCH_2			(String.format(ApiMethod.REST_PREFIX, "search2.view")),
-	GET_PLAYLISTS		(String.format(ApiMethod.REST_PREFIX, "getPlaylists.view")),
-	GET_PLAYLIST		(String.format(ApiMethod.REST_PREFIX, "getPlaylist.view")),
-	CREATE_PLAYLIST		(String.format(ApiMethod.REST_PREFIX, "createPlaylist.view")),
-	DELETE_PLAYLIST		(String.format(ApiMethod.REST_PREFIX, "deletePlaylist.view")),
-	DOWNLOAD			(String.format(ApiMethod.REST_PREFIX, "download.view")),
-	STREAM				(String.format(ApiMethod.REST_PREFIX, "stream.view")),
-	GET_COVER_ART		(String.format(ApiMethod.REST_PREFIX, "getCoverArt.view")),
-	GET_ALBUM_LIST		(String.format(ApiMethod.REST_PREFIX, "getAlbumList.view")),
-	GET_RANDOM_SONGS	(String.format(ApiMethod.REST_PREFIX, "getRandomSongs.view")),
-	GET_PODCASTS		(String.format(ApiMethod.REST_PREFIX, "getPodcasts.view")),
-	REFRESH_PODCASTS	(String.format(ApiMethod.REST_PREFIX, "refreshPodcasts.view")),
-	CREATE_PODCAST		(String.format(ApiMethod.REST_PREFIX, "createPodcastChannel.view")),
-	DELETE_PODCAST		(String.format(ApiMethod.REST_PREFIX, "deletePodcastChannel.view")),
-	SET_RATING			(String.format(ApiMethod.REST_PREFIX, "setRating.view")),
-	STAR				(String.format(ApiMethod.REST_PREFIX, "star.view")),
-	UNSTAR				(String.format(ApiMethod.REST_PREFIX, "unstar.view")),
-	GET_STARRED			(String.format(ApiMethod.REST_PREFIX, "getStarred.view"));
+	PING				(new Version(1, 0, 0), String.format(ApiMethod.REST_PREFIX, "ping.view")),
+	GET_LICENSE			(new Version(1, 0, 0), String.format(ApiMethod.REST_PREFIX, "getLicense.view")),
+	GET_MUSIC_FOLDERS	(new Version(1, 0, 0), String.format(ApiMethod.REST_PREFIX, "getMusicFolders.view")),
+	GET_INDEXES			(new Version(1, 0, 0), String.format(ApiMethod.REST_PREFIX, "getIndexes.view")),
+	GET_MUSIC_DIRECTORY	(new Version(1, 0, 0), String.format(ApiMethod.REST_PREFIX, "getMusicDirectory.view")),
+	SEARCH_2			(new Version(1, 4, 0), String.format(ApiMethod.REST_PREFIX, "search2.view")),
+	GET_PLAYLISTS		(new Version(1, 0, 0), String.format(ApiMethod.REST_PREFIX, "getPlaylists.view")),
+	GET_PLAYLIST		(new Version(1, 0, 0), String.format(ApiMethod.REST_PREFIX, "getPlaylist.view")),
+	CREATE_PLAYLIST		(new Version(1, 2, 0), String.format(ApiMethod.REST_PREFIX, "createPlaylist.view")),
+	DELETE_PLAYLIST		(new Version(1, 2, 0), String.format(ApiMethod.REST_PREFIX, "deletePlaylist.view")),
+	DOWNLOAD			(new Version(1, 0, 0), String.format(ApiMethod.REST_PREFIX, "download.view")),
+	STREAM				(new Version(1, 0, 0), String.format(ApiMethod.REST_PREFIX, "stream.view")),
+	GET_COVER_ART		(new Version(1, 0, 0), String.format(ApiMethod.REST_PREFIX, "getCoverArt.view")),
+	GET_ALBUM_LIST		(new Version(1, 2, 0), String.format(ApiMethod.REST_PREFIX, "getAlbumList.view")),
+	GET_RANDOM_SONGS	(new Version(1, 2, 0), String.format(ApiMethod.REST_PREFIX, "getRandomSongs.view")),
+	GET_PODCASTS		(new Version(1, 6, 0), String.format(ApiMethod.REST_PREFIX, "getPodcasts.view")),
+	REFRESH_PODCASTS	(new Version(1, 9, 0), String.format(ApiMethod.REST_PREFIX, "refreshPodcasts.view")),
+	CREATE_PODCAST		(new Version(1, 9, 0), String.format(ApiMethod.REST_PREFIX, "createPodcastChannel.view")),
+	DELETE_PODCAST		(new Version(1, 9, 0), String.format(ApiMethod.REST_PREFIX, "deletePodcastChannel.view")),
+	SET_RATING			(new Version(1, 6, 0), String.format(ApiMethod.REST_PREFIX, "setRating.view")),
+	STAR				(new Version(1, 8, 0), String.format(ApiMethod.REST_PREFIX, "star.view")),
+	UNSTAR				(new Version(1, 8, 0), String.format(ApiMethod.REST_PREFIX, "unstar.view")),
+	GET_STARRED			(new Version(1, 8, 0), String.format(ApiMethod.REST_PREFIX, "getStarred.view")),
+	GET_BOOKMARKS		(new Version(1, 9, 0), String.format(ApiMethod.REST_PREFIX, "getBookmarks.view")),
+	CREATE_BOOKMARK		(new Version(1, 9, 0), String.format(ApiMethod.REST_PREFIX, "createBookmark.view")),
+	DELETE_BOOKMARK		(new Version(1, 9, 0), String.format(ApiMethod.REST_PREFIX, "deleteBookmark.view"));
 	
 	private static final String REST_PREFIX	= "/rest/%s";
 	
 	private String path;
+	private Version version;
 	
-	private ApiMethod(String path) {
+	private ApiMethod(Version version, String path) {
 		this.path = path;
 	}
 	
 	@Override
 	public String toString() {
 		return this.path;
+	}
+	
+	public Version getVersion() {
+		return this.version;
 	}
 	
 }
