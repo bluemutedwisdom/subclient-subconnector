3c0aa47c0d735c83a897e1691643887acc9dab6a
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-01-01 10:45:59 +0100

    Created interface wrapping Subsonic API playlist handling methods

--- src/main/java/net/subclient/subsonic/Connection.java
+++ src/main/java/net/subclient/subsonic/Connection.java
@@ -25,19 +25,17 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.List;
 
 import javax.xml.ws.http.HTTPException;
 
 import net.subclient.subsonic.api.BrowseAPI;
 import net.subclient.subsonic.api.MediaListAPI;
+import net.subclient.subsonic.api.PlaylistsAPI;
 import net.subclient.subsonic.api.SearchAPI;
 import net.subclient.subsonic.api.SystemAPI;
 import net.subclient.subsonic.exceptions.CompatibilityException;
 import net.subclient.subsonic.exceptions.InvalidResponseException;
 import net.subclient.subsonic.exceptions.SubsonicException;
-import net.subclient.subsonic.responses.GetPlaylistResponse;
-import net.subclient.subsonic.responses.GetPlaylistsResponse;
 import net.subclient.subsonic.responses.GetPodcastResponse;
 import net.subclient.subsonic.responses.GetPodcastsResponse;
 import net.subclient.subsonic.responses.SubsonicResponse;
@@ -50,57 +48,7 @@ import com.google.gson.JsonSyntaxException;
  * @author Alejandro Celaya Alastrué
  * @see <a href="http://www.alejandrocelaya.com">www.alejandrocelaya.com</a>
  */
-public interface Connection extends SystemAPI, BrowseAPI, MediaListAPI, SearchAPI {
-	
-	/**
-     * Gets the playlists available for the current user
-     * @return A {@link net.subclient.subsonic.responses.GetPlaylistsResponse GetPlaylistsResponse} object with the playlists available
-     * @throws SubsonicException If a Subsonic error occurs
-     * @throws IOException 
-     * @throws JsonSyntaxException 
-     * @throws InvalidResponseException If the Subsonic servers returns a non parseable response 
-     * @throws HTTPException If the server response code is other than 200 
-     */
-	public GetPlaylistsResponse getPlaylists() throws JsonSyntaxException, IOException, SubsonicException, InvalidResponseException, HTTPException;
-	/**
-     * Gets information about a specific playlist including songs list on that playlist
-     * @param playlistId ID of the playlist to be returned
-     * @return A {@link net.subclient.subsonic.responses.GetPlaylistResponse GetPlaylistResponse} object with information about the specified playlist
-     * @throws SubsonicException If a Subsonic error occurs
-     * @throws IOException 
-     * @throws JsonSyntaxException 
-     * @throws InvalidResponseException If the Subsonic servers returns a non parseable response 
-     * @throws HTTPException If the server response code is other than 200 
-     */
-	public GetPlaylistResponse getPlaylist(String playlistId) throws JsonSyntaxException, IOException, SubsonicException, InvalidResponseException, HTTPException;
-	
-	/**
-     * Creates a new playlist with the specified songs list and name
-     * @param songsList List of IDs of songs that will form the playlist
-     * @param name Name to be asigned to the playlist
-     * @return A {@link net.subclient.subsonic.responses.SubsonicResponse SubsonicResponse} object
-     * @throws SubsonicException If a Subsonic error occurs
-     * @throws IOException 
-     * @throws JsonSyntaxException 
-     * @throws InvalidResponseException If the Subsonic servers returns a non parseable response 
-     * @throws CompatibilityException If this method is not compatible with the current server 
-     * @throws HTTPException If the server response code is other than 200 
-     */
-	public SubsonicResponse createPlaylist(List<String> songsList, String name) 
-	    		throws JsonSyntaxException, IOException, SubsonicException, InvalidResponseException, CompatibilityException, HTTPException;
-	 
-	/**
-     * Deletes the specified playlist
-     * @param playlistId ID of the playlist to be deleted
-     * @return A {@link net.subclient.subsonic.responses.SubsonicResponse SubsonicResponse} object
-     * @throws SubsonicException If a Subsonic error occurs
-     * @throws IOException 
-     * @throws JsonSyntaxException 
-     * @throws InvalidResponseException If the Subsonic servers returns a non parseable response 
-     * @throws CompatibilityException If this method is not compatible with the current server 
-     * @throws HTTPException If the server response code is other than 200 
-     */
-	public SubsonicResponse deletePlaylist(String playlistId) throws JsonSyntaxException, IOException, SubsonicException, InvalidResponseException, CompatibilityException, HTTPException;
+public interface Connection extends SystemAPI, BrowseAPI, MediaListAPI, SearchAPI, PlaylistsAPI {
 	
 	/**
      * Gets the list of podcasts in the server
